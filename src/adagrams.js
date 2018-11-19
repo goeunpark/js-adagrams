01bce2ed6eaa4db9f57ec572bd2502f4d7ab4ca9
Goeun Park <goeuntothepark@gmail.com>
2018-11-19 00:05:26 -0800

    create drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -13,6 +13,34 @@
 // end
 // return letter_array
 // end
+const scoreChart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
 
 const Adagrams = {
   drawLetters() {
@@ -44,43 +72,28 @@ const Adagrams = {
       Y: 2,
       Z: 1
     };
+    const letterArray = [];
+    const randomTen = [];
 
-    const scoreChart = {
-      A: 1,
-      B: 3,
-      C: 3,
-      D: 2,
-      E: 1,
-      F: 4,
-      G: 2,
-      H: 4,
-      I: 1,
-      J: 8,
-      K: 5,
-      L: 1,
-      M: 3,
-      N: 1,
-      O: 1,
-      P: 3,
-      Q: 10,
-      R: 1,
-      S: 1,
-      T: 1,
-      U: 1,
-      V: 4,
-      W: 4,
-      X: 8,
-      Y: 4,
-      Z: 10
-    };
-
-    const letter_array = [];
-    for(const letter in pool ){
-      for(let i = 0; i < pool[letter]; i ++){
+    for ( const letter in pool ){
+      for ( let i = 0; i < pool[letter]; i ++ ){
         console.log(letter);
-        letter_array.push(letter);
+        letterArray.push(letter);
       }
     }
+
+    for ( let i = 0; i < 10; i ++){
+      let selectLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
+
+      for ( let i = 0; i < letterArray.length-1; i++){
+        if ( letterArray[i] == selectLetter ) {
+          randomTen.push(letterArray[i]);
+          letterArray.splice(i, 1);
+          break;
+        }
+      }
+    }
+    return randomTen;
   },
 
   usesAvailableLetters() {
